5a3082d4d2547d38271451be3b2f7d4ed871d0c0
Deakalka <fedorushun.borya@gmail.com>
2024-03-08 08:30:06 +0200

    +-

--- gallery.js
+++ gallery.js
@@ -20,34 +20,3 @@ document.addEventListener("DOMContentLoaded", function () {
     }, sections.length * 300);
 });
 
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     // Отримуємо всі посилання, які вказують на секції
-//     const links = document.querySelectorAll('a[href^="#"]');
-
-//     // Додаємо обробник події для кожного посилання
-//     links.forEach(link => {
-//         link.addEventListener("click", function (event) {
-//             event.preventDefault(); // Забороняємо стандартну поведінку посилання
-
-//             const targetId = this.getAttribute("href").substring(1); // Отримуємо ідентифікатор цільової секції
-//             const targetSection = document.getElementById(targetId); // Знаходимо цільову секцію
-
-//             if (targetSection) {
-//                 // Викликаємо функцію плавного скролінгу
-//                 scrollToSection(targetSection);
-//             }
-//         });
-//     });
-
-//     // Функція для плавного скролінгу до елементу
-//     function scrollToSection(targetElement) {
-//         const offsetTop = targetElement.offsetTop; // Отримуємо вертикальну позицію елементу відносно верхнього краю
-
-//         window.scrollTo({
-//             top: offsetTop,
-//             behavior: "smooth" // Встановлюємо плавний ефект прокрутки
-//         });
-//     }
-// });
--- mobile_menu.js
+++ mobile_menu.js
@@ -1,52 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const body = document.body;
   const menuButton = document.querySelector('.mobile-menu-button');
   const mobileMenu = document.querySelector('.mob_menu');
   const menuLinks = document.querySelectorAll('.mob_menu .header-link');
-
-  if (menuButton && mobileMenu) {
-    menuButton.addEventListener('click', function() {
-      mobileMenu.classList.toggle('visible');
-    });
-
-    menuLinks.forEach(link => {
-      link.addEventListener('click', function(event) {
-        event.preventDefault();
-
-        const targetId = this.getAttribute('href');
-        const targetSection = document.querySelector(targetId);
-
-        if (targetSection) {
-          mobileMenu.classList.remove('visible');
-
-          setTimeout(() => {
-            targetSection.scrollIntoView({
-              behavior: 'smooth'
-            });
-          }, 300); // Час збігається з часом анімації в CSS (0.3s * 1000 = 300ms)
-        }
-      });
-    });
-  } else {
-    console.error('Не вдалося знайти кнопку або меню.');
-  }
-});
-  
-
-document.addEventListener('DOMContentLoaded', function() {
-  const menuButton = document.querySelector('.mobile-menu-button');
   const menuSvg = document.querySelector('.mobile-menu-svg use');
+  const scrollButton = document.querySelector('.scroll-to-top-btn');
+
+  if (!menuButton || !mobileMenu || !menuSvg || !scrollButton) {
+    console.error('Не вдалося знайти кнопку, меню або іконку.');
+    return;
+  }
+
+  function openMenu() {
+    mobileMenu.classList.add('visible');
+    body.style.overflow = 'hidden';
+    
+    if (menuSvg.getAttribute('href') === './img/icons.svg#icon-mobile_menu') {
+      menuSvg.setAttribute('href', './img/icons.svg#btn-icon-mob');
+    } else {
+      menuSvg.setAttribute('href', './img/icons.svg#icon-mobile_menu');
+    }
+  }
+
+  function closeMenu() {
+    mobileMenu.classList.remove('visible');
+    body.style.overflow = 'auto';
+  }
 
   menuButton.addEventListener('click', function() {
-    if (menuSvg.getAttribute('href') === './img/icons.svg#icon-mobile_menu') {
-      menuSvg.setAttribute('href', './img/icons.svg#btn-icon-mob'); // Замініть шлях на вашу нову SVG іконку
+    if (mobileMenu.classList.contains('visible')) {
+      closeMenu();
     } else {
-      menuSvg.setAttribute('href', './img/icons.svg#icon-mobile_menu'); // Повернення до початкової іконки
+      openMenu();
     }
   });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  const menuLinks = document.querySelectorAll('.header-link');
 
   menuLinks.forEach(link => {
     link.addEventListener('click', function(event) {
@@ -56,32 +43,25 @@ document.addEventListener('DOMContentLoaded', function() {
       const targetSection = document.querySelector(targetId);
 
       if (targetSection) {
-        targetSection.scrollIntoView({
-          behavior: 'smooth'
-        });
+        closeMenu();
+
+        setTimeout(() => {
+          targetSection.scrollIntoView({
+            behavior: 'smooth'
+          });
+        }, 300);
       }
     });
   });
-});
 
+  window.addEventListener('scroll', function() {
+    scrollButton.style.display = (window.pageYOffset > 100) ? 'block' : 'none';
+  });
 
-
-// ======== 
-
-
-const scrollButton = document.querySelector('.scroll-to-top-btn');
-
-window.addEventListener('scroll', function() {
-  if (window.pageYOffset > 100) { // Відстань, після якої кнопка з'являється
-    scrollButton.style.display = 'block';
-  } else {
-    scrollButton.style.display = 'none';
-  }
-});
-
-scrollButton.addEventListener('click', function() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
+  scrollButton.addEventListener('click', function() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   });
 });
